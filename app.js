28164343e4eb16245c851de1784b8155b5f14e90
lifeforme <siyaga123@gmail.com>
2022-07-07 09:38:48 +0700

    membuat Pertanyaan nama, noHP, Email

--- app.js
+++ app.js
@@ -19,9 +19,20 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Membuat pertanyaan menggunakan question 
-rl.question('What is your name? ', (name)=> {
-    console.log(`Thank you ${name}`);
+// // Membuat pertanyaan menggunakan question 
+// rl.question('What is your name? ', (name)=> {
+//     console.log(`Thank you ${name}`);
 
-    rl.close()
+//     rl.close();
+// });
+
+
+// membuat Pertanyaaan Nama, NoHP, Email
+rl.question('What your name? ',(name) => {
+   rl.question('What your number phone? ', (nohp)=> {
+    rl.question('what your Email? ',(email)=> {
+        console.log(`Hatur Thank You ${name}, Your Number is ${nohp}, Your Email is ${email} `);
+        rl.close();
+    });
+   });
 });
