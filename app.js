63ffb9a86c9b4cc305f2c96ebacdb457e58ae32e
lifeforme <siyaga123@gmail.com>
2022-07-07 09:24:22 +0700

    Membaca Isi file

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const fs = require('fs');
 
 
 // menulis string ke file (Synchronous)
-fs.writeFileSync('test.txt','Hello World secara synchronous!');
+// fs.writeFileSync('test.txt','Hello World secara synchronous!');
+
+// Membaca isi file (Ansynchronous)
+fs.readFile('test.txt','utf-8', (err,data) => {
+    if(err) throw err;
+    console.log(data);
+});
