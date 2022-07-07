aa6c21f19be6c3069bb58ad6ee817aa2550daa1c
lifeforme <siyaga123@gmail.com>
2022-07-07 09:30:26 +0700

    Membuat readline Input dan output

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // core module
 // file system
 const fs = require('fs');
+const readline = require('readline');
 
 
 // menulis string ke file (Synchronous)
 // fs.writeFileSync('test.txt','Hello World secara synchronous!');
 
 // Membaca isi file (Ansynchronous)
-fs.readFile('test.txt','utf-8', (err,data) => {
-    if(err) throw err;
-    console.log(data);
+// fs.readFile('test.txt','utf-8', (err,data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+// Membuat object readline input dan output
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Membuat pertanyaan menggunakan question 
+rl.question('What is your name? ', (name)=> {
+    console.log(`Thank you ${name}`);
+
+    rl.close()
 });
